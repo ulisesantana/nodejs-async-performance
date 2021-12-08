7b3b920feeab0ad5573d99f8cc12df4453cd492c
Ulises Santana <ulisesantana@gmail.com>
2021-12-08 09:38:01 +0000

    ✅ add demo for cached promises

--- demo/cached-promises.mjs
+++ demo/cached-promises.mjs
@@ -1,40 +1,41 @@
-import {Benchmark} from "./helpers/benchmark.mjs";
-import {setTimeout} from "timers/promises"
+import { Benchmark } from "./helpers/benchmark.mjs";
+import { setTimeout } from "timers/promises"
 
 async function doSomethingAsync() {
-  await setTimeout(10)
+  await setTimeout(1)
   return 42
 }
 
-class CacheByValue {
-  #value
+function CacheByValue() {
+  let value = undefined
 
-  async getNumber() {
-    if(this.#value === undefined) {
-      this.#value = await doSomethingAsync()
+  return {
+    async getNumber() {
+      if (value === undefined) {
+        value = await doSomethingAsync()
+      }
+      return value
     }
-    return this.#value
   }
 }
 
-class CacheByPromise {
-  #value
+function CacheByPromise() {
+  let value = undefined
 
-  getNumber() {
-    if(this.#value === undefined) {
-      this.#value = doSomethingAsync()
+  return {
+    getNumber() {
+      if (value === undefined) {
+        value = doSomethingAsync()
+      }
+      return value
     }
-    return this.#value
   }
 }
-
-const byPromise = new CacheByPromise()
-const byValue = new CacheByValue()
 
 const suite = new Benchmark('Caching promises')
 
-await suite.add('by value', byPromise.getNumber.bind(byPromise))
+await suite.add('by value', CacheByValue().getNumber)
+await suite.add('by promise', CacheByPromise().getNumber)
 
-await suite.add('by promise', byValue.getNumber.bind(byValue))
 
 suite.printResults()
