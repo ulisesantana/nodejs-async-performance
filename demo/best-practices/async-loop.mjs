4733c7a42d674d6630046de9e5a3624bf62202fc
Ulises Santana <ulisesantana@gmail.com>
2022-04-30 19:06:58 +0100

    📝 update async-loop demo

--- demo/best-practices/async-loop.mjs
+++ demo/best-practices/async-loop.mjs
@@ -1,23 +1,29 @@
-import {measure, formatNumber} from "../_helpers/index.mjs";
 import {setTimeout} from 'timers/promises'
+import {createHash} from 'crypto'
+import {measure, formatNumber} from "../_helpers/index.mjs";
 import {asyncIteration} from "../_helpers/async-iteration.mjs";
 
-async function doSomethingAsync() {
+async function fetchUserInfo(id) {
   await setTimeout(1)
-  return Math.random()
+  return id.toString().substring(0,4)
 }
 
+const ids = Array.from({length: 100}).map(() => createHash('sha256'))
 const amountOfExecutions = 1000
 console.log(`Async loops for ${formatNumber(amountOfExecutions)} executions`)
 
-await measure('using Promise.all', () => asyncIteration(amountOfExecutions, () => {
-  return Promise.all([...Array(100).keys()].map(doSomethingAsync))
+await measure('using Promise.all', () => asyncIteration(amountOfExecutions, async () => {
+  const values = []
+  for (const id of ids) {
+    values.push(fetchUserInfo(id))
+  }
+  return Promise.all(values)
 }))
 
 await measure('using an await inside a for', () => asyncIteration(amountOfExecutions, async () => {
   const values = []
-  for (const _index of Array(100).keys()) {
-    values.push(await doSomethingAsync())
+  for (const id of ids) {
+    values.push(await fetchUserInfo(id))
   }
   return values
 }))
